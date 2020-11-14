bda0892ac284654e0263a463628fa83345a48b9e
iamrajendra <rajendrarajaram@hotmail.com>
2020-11-14 08:47:49 +0530

    Implement the map and connect it with firebase map

--- src/app/persecmap/persecmap.component.ts
+++ src/app/persecmap/persecmap.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FireServiceService} from '../fire-service.service'
 
 @Component({
   selector: 'app-persecmap',
@@ -6,11 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./persecmap.component.css']
 })
 export class PersecmapComponent implements OnInit {
-  YOUR_API_KEY:string ="AIzaSyDeexU6v72IvsZqLsCFaFQuogP3PTCByRY"
-  constructor() { }
+  title = 'Location on Map';
+  lat = 20.0057155;
+  lng = 64.3864263;
+  previous;
 
-  ngOnInit(): void {
-  }
+  locations:Location[];
+
+
+  constructor( private fire :FireServiceService) { }
 
   
+  ngOnInit(): void {
+    this.getLocationList();
+    
+
+  }
+  getLocationList() {
+    this.fire.getLocations().subscribe(data => {
+      this.locations = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          ...(e.payload.doc.data()) as Location
+        } as Location;
+
+  });
+
+});
+}
+
+clickedMarker(infowindow) {
+  if (this.previous) {
+    this.previous.close();
+  }
+  this.previous = infowindow;
+}
+
 }
